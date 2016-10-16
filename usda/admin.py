cbc40c64327b2b65e3030f5dd9225a8eaac6e3a9
Onat <onatbas@gmail.com>
2016-10-16 22:50:24 +0300

    New views

--- usda/admin.py
+++ usda/admin.py
@@ -4,3 +4,5 @@ from .models import *
 admin.site.register(Nutrient)
 admin.site.register(Food)
 admin.site.register(Nutrient_ingredient)
+admin.site.register(Search_query)
+admin.site.register(Search_entry)
--- usda/models.py
+++ usda/models.py
@@ -32,14 +32,14 @@ class Nutrient_ingredient(models.Model):
 class Search_query(models.Model):
 	query_string = models.CharField(max_length=100)
 	def __str__(self):
-		return query_string
+		return self.query_string
 
 #--------------------------------------------------------------
 class Search_entry(models.Model):
 	food = models.ForeignKey(Food, on_delete=models.CASCADE)
 	query = models.ForeignKey(Search_query, on_delete=models.CASCADE)
 
-
+#--------------------------------------------------------------
 def create_food_from_json(json):
 	object = Food()
 	object.name = json['name']
@@ -48,12 +48,31 @@ def create_food_from_json(json):
 	object.save()
 	return object
 
+#--------------------------------------------------------------
 def get_food_from_json(json):
 	results = Food.objects.filter(ndbno=json['ndbno'])
 	if (len(results) == 0):
 		return create_food_from_json(json)
 	else:
 		return results[0]
+
+
+#--------------------------------------------------------------
+def register_for_queries(objects, name):
+	object = Search_query()
+	object.query_string = name
+	object.save()
+	length = len(objects)
+	
+	result_objects = []
+	for i in range(0, length):
+		entry = Search_entry()
+		entry.query = object
+		no = objects[i]['ndbno']
+		entry.food = Food.objects.filter(ndbno=no)[0]
+		entry.save()
+		result_objects.append(entry)
+	return result_objects
 
 #This class serves as the main interface for interacting with USDA api.
 #--------------------------------------------------------------
@@ -63,17 +82,24 @@ class USDA:
 		return 'http://api.nal.usda.gov/ndb/search/?format=json&q=' + food_name + '&max=100&offset=0&api_key=' + self.api_key
 	def search_by_name(self, name):
 		result = Search_query.objects.filter(query_string=name)
+		objects = []
 		if (len(result) != 0):
-			return result
+			objects = result[0].search_entry_set.all()
 		else:
 			url = self.get_search_url(name)
+			
 			json_response = requests.get(url).json()['list']['item']
 			length = len(json_response)
-			if length == 0:
-				return ''
-			else:
-				print(length)
-				objects = []
-				for i in range(0, length):
-					objects.append(get_food_from_json(json_response[i]))
-				return objects
+			for i in range(0, length):
+				get_food_from_json(json_response[i])
+			objects = register_for_queries(json_response, name)
+		print('Returning')
+		print (len(objects))
+		return objects
+
+
+
+
+
+
+
